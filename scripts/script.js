d035eb65870260b0ec9015bd089eb66ee97ca8d1
Carlos Vara <carlos.vara@alumnos.udg.mx>
2018-11-13 00:45:33 -0600

    Adding login & sign-up windows

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,7 @@
  * @Author: schwarze_falke
  * @Date:   2018-11-12T22:21:32-06:00
  * @Last modified by:   schwarze_falke
- * @Last modified time: 2018-11-12T22:54:24-06:00
+ * @Last modified time: 2018-11-13T00:29:13-06:00
  */
  // Get the modal
  var modal = document.getElementById('myModal');
@@ -45,3 +45,47 @@
    open: function(){},               // Function: Open callback
    close: function(){}               // Function: Close callback
  });
+ /* ------------------------------------ Click on login and Sign Up to  changue and view the effect
+ ---------------------------------------
+ */
+
+ function cambiar_login() {
+   document.querySelector('.cont_forms').className = "cont_forms cont_forms_active_login";
+ document.querySelector('.cont_form_login').style.display = "block";
+ document.querySelector('.cont_form_sign_up').style.opacity = "0";
+
+ setTimeout(function(){  document.querySelector('.cont_form_login').style.opacity = "1"; },400);
+
+ setTimeout(function(){
+ document.querySelector('.cont_form_sign_up').style.display = "none";
+ },200);
+   }
+
+ function cambiar_sign_up(at) {
+   document.querySelector('.cont_forms').className = "cont_forms cont_forms_active_sign_up";
+   document.querySelector('.cont_form_sign_up').style.display = "block";
+ document.querySelector('.cont_form_login').style.opacity = "0";
+
+ setTimeout(function(){  document.querySelector('.cont_form_sign_up').style.opacity = "1";
+ },100);
+
+ setTimeout(function(){   document.querySelector('.cont_form_login').style.display = "none";
+ },400);
+
+
+ }
+
+
+
+ function ocultar_login_sign_up() {
+
+ document.querySelector('.cont_forms').className = "cont_forms";
+ document.querySelector('.cont_form_sign_up').style.opacity = "0";
+ document.querySelector('.cont_form_login').style.opacity = "0";
+
+ setTimeout(function(){
+ document.querySelector('.cont_form_sign_up').style.display = "none";
+ document.querySelector('.cont_form_login').style.display = "none";
+ },500);
+
+   }
