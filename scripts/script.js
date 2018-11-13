7c60e0ae9b0194e4f2659fd2a94ef2fe6fb89a6e
Carlos Vara <carlos.vara@alumnos.udg.mx>
2018-11-13 01:47:51 -0600

    Adding map

--- scripts/script.js
+++ scripts/script.js
@@ -2,33 +2,8 @@
  * @Author: schwarze_falke
  * @Date:   2018-11-12T22:21:32-06:00
  * @Last modified by:   schwarze_falke
- * @Last modified time: 2018-11-13T00:29:13-06:00
+ * @Last modified time: 2018-11-13T01:33:05-06:00
  */
- // Get the modal
- var modal = document.getElementById('myModal');
-
- // Get the button that opens the modal
- var btn = document.getElementById("myBtn");
-
- // Get the <span> element that closes the modal
- var span = document.getElementsByClassName("close")[0];
-
- // When the user clicks on the button, open the modal
- btn.onclick = function() {
-     modal.style.display = "block";
- }
-
- // When the user clicks on <span> (x), close the modal
- span.onclick = function() {
-     modal.style.display = "none";
- }
-
- // When the user clicks anywhere outside of the modal, close it
- window.onclick = function(event) {
-     if (event.target == modal) {
-         modal.style.display = "none";
-     }
- }
 
  var navigation = responsiveNav(".nav-collapse", {
    animate: true,                    // Boolean: Use CSS3 transitions, true or false
@@ -74,8 +49,6 @@
 
 
  }
-
-
 
  function ocultar_login_sign_up() {
 
