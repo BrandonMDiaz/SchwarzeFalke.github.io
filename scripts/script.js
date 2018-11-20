f42d1d7773f30b7d04998c7bf43723c19cc88533
Carlos Vara <carlos.vara@alumnos.udg.mx>
2018-11-20 02:12:25 -0600

    Adding colors to the page!

--- scripts/script.js
+++ scripts/script.js
@@ -2,63 +2,123 @@
  * @Author: schwarze_falke
  * @Date:   2018-11-12T22:21:32-06:00
  * @Last modified by:   schwarze_falke
- * @Last modified time: 2018-11-13T01:33:05-06:00
+ * @Last modified time: 2018-11-20T01:59:43-06:00
  */
+ var menuItems = [].slice.call(document.querySelectorAll('.menu__item')),
+ menuSubs = [].slice.call(document.querySelectorAll('.dropdown-menu')),
+ selectedMenu = undefined,
+ subBg = document.querySelector('.dropdown__bg'),
+ subBgBtm = document.querySelector('.dropdown__bg-bottom'),
+ subArr = document.querySelector('.dropdown__arrow'),
+ subCnt = document.querySelector('.dropdown__wrap'),
+ header = document.querySelector('.main-header'),
+ closeDropdownTimeout,
 
- var navigation = responsiveNav(".nav-collapse", {
-   animate: true,                    // Boolean: Use CSS3 transitions, true or false
-   transition: 284,                  // Integer: Speed of the transition, in milliseconds
-   label: "Menu",                    // String: Label for the navigation toggle
-   insert: "before",                  // String: Insert the toggle before or after the navigation
-   customToggle: "",                 // Selector: Specify the ID of a custom toggle
-   closeOnNavClick: false,           // Boolean: Close the navigation when one of the links are clicked
-   openPos: "relative",              // String: Position of the opened nav, relative or static
-   navClass: "nav-collapse",         // String: Default CSS class. If changed, you need to edit the CSS too!
-   navActiveClass: "js-nav-active",  // String: Class that is added to <html> element when nav is active
-   jsClass: "js",                    // String: 'JS enabled' class which is added to <html> element
-   init: function(){},               // Function: Init callback
-   open: function(){},               // Function: Open callback
-   close: function(){}               // Function: Close callback
+ startCloseTimeout = function startCloseTimeout() {
+ 	closeDropdownTimeout = setTimeout(function () {return closeDropdown();}, 50);
+ },
+
+ stopCloseTimeout = function stopCloseTimeout() {
+ 	clearTimeout(closeDropdownTimeout);
+ },
+
+ openDropdown = function openDropdown(el) {
+
+ 	//- get menu ID
+ 	var menuId = el.getAttribute('data-sub');
+ 	//- get related sub menu
+ 	var menuSub = document.querySelector('.dropdown-menu[data-sub="' + menuId + '"]');
+ 	//- get menu sub content
+ 	var menuSubCnt = menuSub.querySelector('.dropdown-menu__content');
+ 	//- get bottom section of current sub
+ 	var menuSubBtm = menuSubCnt.querySelector('.bottom-section').getBoundingClientRect();
+ 	//- get height of top section
+ 	var menuSubTop = menuSubCnt.querySelector('.top-section').getBoundingClientRect();
+ 	//- get menu position
+ 	var menuMeta = el.getBoundingClientRect();
+ 	//- get sub menu position
+ 	var subMeta = menuSubCnt.getBoundingClientRect();
+
+
+ 	//- set selected menu
+ 	selectedMenu = menuId;
+
+
+ 	//- Remove active Menu
+ 	menuItems.forEach(function (el) {return el.classList.remove('active');});
+ 	//- Set current menu to active
+ 	el.classList.add('active');
+
+ 	//- Remove active sub menu
+ 	menuSubs.forEach(function (el) {return el.classList.remove('active');});
+ 	//- Set current menu to active
+ 	menuSub.classList.add('active');
+
+ 	//- Set dropdown menu background style to match current submenu style
+ 	subBg.style.opacity = 1;
+ 	subBg.style.left = menuMeta.left - (subMeta.width / 2 - menuMeta.width / 2) + 'px';
+ 	subBg.style.width = subMeta.width + 'px';
+ 	subBg.style.height = subMeta.height + 'px';
+ 	//- Set dropdown menu bottom section background position
+ 	subBgBtm.style.top = menuSubTop.height + 'px';
+ 	console.log(menuSubBtm);
+
+ 	//- Set Arrow position
+ 	subArr.style.opacity = 1;
+ 	subArr.style.left = menuMeta.left + menuMeta.width / 2 - 10 + 'px';
+
+ 	//- Set sub menu style
+ 	subCnt.style.opacity = 1;
+ 	subCnt.style.left = menuMeta.left - (subMeta.width / 2 - menuMeta.width / 2) + 'px';
+ 	subCnt.style.width = subMeta.width + 'px';
+ 	subCnt.style.height = subMeta.height + 'px';
+
+ 	//- Set current sub menu style
+ 	menuSub.style.opacity = 1;
+
+ 	header.classList.add('dropdown-active');
+
+ },
+ closeDropdown = function closeDropdown() {
+
+ 	//- Remove active class from all menu items
+ 	menuItems.forEach(function (el) {return el.classList.remove('active');});
+ 	//- Remove active class from all sub menus
+ 	menuSubs.forEach(function (el) {
+ 		el.classList.remove('active');
+ 		el.style.opacity = 0;
+ 	});
+ 	//- set sub menu background opacity
+ 	subBg.style.opacity = 0;
+ 	//- set arrow opacity
+ 	subArr.style.opacity = 0;
+
+
+ 	// unset selected menu
+ 	selectedMenu = undefined;
+
+ 	header.classList.remove('dropdown-active');
+ };
+
+
+ //- Binding mouse event to each menu items
+ menuItems.forEach(function (el) {
+
+ 	//- mouse enter event
+ 	el.addEventListener('mouseenter', function () {
+ 		stopCloseTimeout();
+ 		openDropdown(this);
+ 	}, false);
+
+ 	//- mouse leave event
+ 	el.addEventListener('mouseleave', function () {return startCloseTimeout();}, false);
+
  });
- /* ------------------------------------ Click on login and Sign Up to  changue and view the effect
- ---------------------------------------
- */
 
- function cambiar_login() {
-   document.querySelector('.cont_forms').className = "cont_forms cont_forms_active_login";
- document.querySelector('.cont_form_login').style.display = "block";
- document.querySelector('.cont_form_sign_up').style.opacity = "0";
+ //- Binding mouse event to each sub menus
+ menuSubs.forEach(function (el) {
 
- setTimeout(function(){  document.querySelector('.cont_form_login').style.opacity = "1"; },400);
+ 	el.addEventListener('mouseenter', function () {return stopCloseTimeout();}, false);
+ 	el.addEventListener('mouseleave', function () {return startCloseTimeout();}, false);
 
- setTimeout(function(){
- document.querySelector('.cont_form_sign_up').style.display = "none";
- },200);
-   }
-
- function cambiar_sign_up(at) {
-   document.querySelector('.cont_forms').className = "cont_forms cont_forms_active_sign_up";
-   document.querySelector('.cont_form_sign_up').style.display = "block";
- document.querySelector('.cont_form_login').style.opacity = "0";
-
- setTimeout(function(){  document.querySelector('.cont_form_sign_up').style.opacity = "1";
- },100);
-
- setTimeout(function(){   document.querySelector('.cont_form_login').style.display = "none";
- },400);
-
-
- }
-
- function ocultar_login_sign_up() {
-
- document.querySelector('.cont_forms').className = "cont_forms";
- document.querySelector('.cont_form_sign_up').style.opacity = "0";
- document.querySelector('.cont_form_login').style.opacity = "0";
-
- setTimeout(function(){
- document.querySelector('.cont_form_sign_up').style.display = "none";
- document.querySelector('.cont_form_login').style.display = "none";
- },500);
-
-   }
+ });
